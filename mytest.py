cd9bd795804a5a3515b1b7ebc586f606a07dd7ed
Hamayun musa <68311576+hamayunpro@users.noreply.github.com>
2021-09-13 07:32:52 +0500

    Add files via upload

---
+++ mytest.py
@@ -0,0 +1,12 @@
+#Logo
+            print("WELCOME TO MY SCRIPT")
+            
+            \033[1;92m FACEBOOK CLONING
+            \033[1;92m VERY FAST TOOOOLS
+            \033[1;92m THANKS FOR USING 
+                        
+            
+           
+           
+           
+           
